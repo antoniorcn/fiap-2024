da94b92253437570295b050b0a612a408b6921c9
Antonio Carvalho <antoniorcn@hotmail.com>
2024-03-08 21:58:25 -0300

    parte 2

--- 2tdspv/aula09/tarefas/App.js
+++ 2tdspv/aula09/tarefas/App.js
@@ -19,13 +19,20 @@ const estilos = StyleSheet.create({
 export default function App() {
   return (
     <View style={estilos.principal}>
-      <View style={{flex: 1}}>
+      <View style={{flex: 2}}>
         <Image source={imgTarefas} resizeMethod="auto" 
-                resizeMode="center"/>
+                resizeMode="cover"/>
         <Text style={estilos.titulo}>Gestão de Tarefas</Text>
       </View>
       <View style={{flex: 3}}>
-
+        <Text>Nome do Tarefa:</Text>
+        <TextInput/>
+        <Text>Data conclusão:</Text>
+        <TextInput/>
+        <Text>Prioridade:</Text>
+        <TextInput/>
+        <Switch value={false}/>
+        <Button title="Salvar"/>
       </View>
 
     </View>
