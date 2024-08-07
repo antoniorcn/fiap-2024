635b1b2d2c6d2d40e5fa7f292bdade18bee610b3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-07 20:57:16 -0300

    2tdspv aula38

---
+++ 2tdspv/aula38/HelloWorld/app/src/main/java/edu/curso/helloworld/HelloWorld.kt
@@ -0,0 +1,18 @@
+package edu.curso.helloworld
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class HelloWorld : Activity() {
+
+    override fun onCreate(bundle : Bundle?){
+        super.onCreate(bundle)
+
+        val txt = TextView( this )
+        txt.text = "Hello World"
+
+        setContentView( txt )
+    }
+
+}
