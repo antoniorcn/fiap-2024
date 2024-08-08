d2cfdbc1cf51c992186deba1030146102365a655
Antonio Carvalho <antoniorcn@hotmail.com>
2024-08-07 23:12:48 -0300

    teste

--- 2tdsr/aula40/HelloWorld/app/src/main/java/edu/curso/helloworld/HelloWorld.kt
+++ 2tdsr/aula40/HelloWorld/app/src/main/java/edu/curso/helloworld/HelloWorld.kt
@@ -2,17 +2,27 @@ package edu.curso.helloworld
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class HelloWorld : Activity() {
-
     override fun onCreate(bundle : Bundle?) {
         super.onCreate(bundle)
-
+        Log.i("HelloWorldApp", "onCreate Ativado")
         val txt = TextView( this )
-        txt.text = "Hello World"
-
+        // txt.setText("Hello World")
+        txt.text = getString(R.string.texto)
         setContentView( txt )
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.i("HelloWorldApp", "onStart Executado")
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("HelloWorldApp", "onResume Executado")
+    }
 }
