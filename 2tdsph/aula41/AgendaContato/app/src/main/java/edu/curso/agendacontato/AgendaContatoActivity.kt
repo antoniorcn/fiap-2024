d7f725760f9840bf2eee902684e9244997f7aec1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-15 09:35:44 -0300

    2tdsph aula41 parte 2

---
+++ 2tdsph/aula41/AgendaContato/app/src/main/java/edu/curso/agendacontato/AgendaContatoActivity.kt
@@ -0,0 +1,12 @@
+package edu.curso.agendacontato
+
+import android.app.Activity
+import android.os.Bundle
+
+class AgendaContatoActivity : Activity() {
+
+    override fun onCreate(bundle : Bundle?) {
+        super.onCreate(bundle)
+        setContentView(R.layout.agenda_contato_layout)
+    }
+}
